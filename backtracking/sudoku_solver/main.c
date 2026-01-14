485f5e26bf95b6f37e699a77657b7e377a010720
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-01-14 18:37:57 +0530

    add helper functions

--- backtracking/sudoku_solver/main.c
+++ backtracking/sudoku_solver/main.c
@@ -4,13 +4,43 @@
 // This program solves sudoku puzzles using backtracking
 // which uses recurrsion, and DFS
 // Based on https://youtu.be/G_UYXzGuqvM (Python Sudoku Solver - Computerphile
-)
 //
 
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+bool is_solved(int grid[9][9]){
+  for (int i = 0; i < 0; i++){
+   for (int j = 0; j < 0; j++){
+    if (grid[i][j] == 0){
+      return false;
+    }
+   }
+  }
+  return true;
+}
+void print_grid(int grid[9][9]){
+  for (int row_i = 0; row_i < 9; row_i++) {
+    if (row_i % 3 == 0 && row_i != 0) {
+      printf("------ ");
+      printf("------ ");
+      printf("------ \n");
+    }
 
+    for (int col_i = 0; col_i < 9; col_i++) {
+      if (col_i % 3 == 0 && col_i != 0) {
+        printf("| ");
+      }
+      if (grid[row_i][col_i] != 0) {
+        printf("%d ", grid[row_i][col_i]);
+      } else {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+ 
+}
 bool possible (int grid[9][9], int row, int col, int value){
 
   assert(row >= 0);
@@ -57,25 +87,7 @@ int main() {
       {6, 0, 0, 0, 0, 0, 0, 7, 5}, {0, 0, 3, 4, 0, 0, 0, 0, 0},
       {0, 0, 0, 2, 0, 0, 6, 0, 0},
   };
-  for (int row_i = 0; row_i < 9; row_i++) {
-    if (row_i % 3 == 0 && row_i != 0) {
-      printf("------ ");
-      printf("------ ");
-      printf("------ \n");
-    }
-
-    for (int col_i = 0; col_i < 9; col_i++) {
-      if (col_i % 3 == 0 && col_i != 0) {
-        printf("| ");
-      }
-      if (grid[row_i][col_i] != 0) {
-        printf("%d ", grid[row_i][col_i]);
-      } else {
-        printf("  ");
-      }
-    }
-    printf("\n");
-  }
+  print_grid(grid);
   printf("sudoku solver\n");
   return 0;
 }
